3012b2664cf329ca7b8a39b701530b5e709e91ae
Jay Chiarella <jay.chiarella@ultimate.ai>
2022-05-06 10:28:26 +0200

    added new endpoints

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/spaghetti", (req, res) => {
+  res.send("say spaghetti!");
+});
+
+app.post("/spaghetti", (req, res) => {
+  console.log(req.body);
+  res.send("say spaghetti!!");
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
